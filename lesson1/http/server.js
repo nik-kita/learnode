c62dd01d7a20c862ca19ae10b9c3a74d41a22060
nik <nikita.moiseienko@onix-systems.com>
2020-12-14 17:59:55 +0200

    commit

--- lesson1/http/server.js
+++ lesson1/http/server.js
@@ -1,27 +1,27 @@
-const http = require('http');
-const port = 9898;
-const server = http.createServer((req, res) => {
-    if(res.toString().length == 0) {
-        res.end('Good night!');
-    }
-    res.write(`It is so noisy around!... You say ${listenInNoise(res.toString())}? May be repeate please...`);
-}).listen(port, (err) => {
-    if(err) {
-        console.log('Ooops...');
-    } else {
-        console.log('Server is listening on port ' + port);
-    }
-});
-
-
-
-
-function listenInNoise(words) {
-    let noise = 's';
-    return [...words].map((ch) => {
-        if(Math.random() > 0.6) {
-            ch += noise;
-            noise += 's';
+    const http = require('http');
+    const port = 9898;
+    const server = http.createServer((req, res) => {
+        if(res.toString().length == 0) {
+            res.end('Good night!');
         }
-    }).join('');
-}
+        res.write(`It is so noisy around!... You say ${listenInNoise(res.toString())}? May be repeate please...`);
+    }).listen(port, (err) => {
+        if(err) {
+            console.log('Ooops...');
+        } else {
+            console.log('Server is listening on port ' + port);
+        }
+    });
+
+
+
+
+    function listenInNoise(words) {
+        let noise = 's';
+        return [...words].map((ch) => {
+            if(Math.random() > 0.6) {
+                ch += noise;
+                noise += 's';
+            }
+        }).join('');
+    }
